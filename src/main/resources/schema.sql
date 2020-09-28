7b78078ddbefdf031ea923a8aab9737a01b7b373
projectormato <k.keita811.anuy@gmail.com>
2020-09-29 00:30:57 +0900

    postgreSQLからMySQLを使用するようにした

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,23 @@
 CREATE TABLE IF NOT EXISTS problem
   (
-     problem_id  SERIAL PRIMARY KEY NOT NULL,
+     problem_id  BIGINT AUTO_INCREMENT PRIMARY KEY,
      title       VARCHAR(100) NOT NULL,
      description VARCHAR(1000) NOT NULL
   );
 
 CREATE TABLE IF NOT EXISTS question
   (
-     question_id SERIAL PRIMARY KEY NOT NULL,
+     question_id BIGINT AUTO_INCREMENT PRIMARY KEY,
      statement   VARCHAR(500) NOT NULL,
-     problem_id  INTEGER NOT NULL,
+     problem_id  BIGINT NOT NULL,
      FOREIGN KEY (problem_id) REFERENCES problem(problem_id)
   );
 
 CREATE TABLE IF NOT EXISTS choice
   (
-     choice_id    SERIAL PRIMARY KEY NOT NULL,
+     choice_id    BIGINT AUTO_INCREMENT PRIMARY KEY,
      content      VARCHAR(100) NOT NULL,
      correct_flag BOOLEAN NOT NULL,
-     question_id  INTEGER NOT NULL,
+     question_id  BIGINT NOT NULL,
      FOREIGN KEY (question_id) REFERENCES question(question_id)
   );
