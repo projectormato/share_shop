9e8ffec78d3ea0cef06a748e85f9d8db1813519e
projectormato <k.keita811.anuy@gmail.com>
2020-10-16 09:27:16 +0900

    feat: ログアウトできるようにした

--- src/main/java/projectormato/ss/config/WebSecurityConfig.kt
+++ src/main/java/projectormato/ss/config/WebSecurityConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +22,12 @@ open class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .oauth2Login()
                 .loginPage("/login")
                 .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutRequestMatcher(AntPathRequestMatcher("/logout**"))
+                .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
                 .and()
                 .csrf()
     }
