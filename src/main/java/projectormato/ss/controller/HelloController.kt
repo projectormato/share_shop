ca7631cac9a68370e8461eec01f59d62b04d9f64
projectormato <k.keita811.anuy@gmail.com>
2021-02-07 21:34:02 +0900

    refactor: HelloController をkt化

--- src/main/java/projectormato/ss/controller/HelloController.kt
+++ src/main/java/projectormato/ss/controller/HelloController.kt
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+class HelloController {
 
-    @GetMapping(path = "/hello")
-    public String hello() {
+    @GetMapping(path = ["/hello"])
+    fun hello(): String {
         return "hello";
     }
-
 }
