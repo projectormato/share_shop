a16bef44a5b23fca42125e7c88d7c2bf5f16af0d
projectormato <k.keita811.anuy@gmail.com>
2021-02-13 22:12:20 +0900

    feat: 利用規約ページのガワを作成

---
+++ src/main/java/projectormato/ss/controller/TermsOfServiceController.kt
@@ -0,0 +1,13 @@
+package projectormato.ss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class TermsOfServiceController {
+
+    @GetMapping(path = ["/terms"])
+    fun hello(): String {
+        return "terms";
+    }
+}
