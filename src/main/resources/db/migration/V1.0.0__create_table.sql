d374b04eabf01dbbc3213823e7740570e3a612f6
projectormato <k.keita811.anuy@gmail.com>
2021-02-28 11:54:30 +0900

    chore: Flyway のデータマイグレーションの仕組みを導入

---
+++ src/main/resources/db/migration/V1.0.0__create_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS shop
+(
+    shop_id  BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id  VARCHAR(100) NOT NULL,
+    url      VARCHAR(200) NOT NULL,
+    `name`   VARCHAR(100) NOT NULL,
+    address  VARCHAR(1000) NOT NULL,
+    hours    VARCHAR(1000) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS share_shared
+(
+    share_shared_id  BIGINT AUTO_INCREMENT PRIMARY KEY,
+    share_id         VARCHAR(100) NOT NULL,
+    shared_id        VARCHAR(100) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS `user`
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id  VARCHAR(100) NOT NULL,
+    email    VARCHAR(100) NOT NULL
+    );
